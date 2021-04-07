7a448b1a7614771faabc280b8fe75c679e903730
saqla <whitecross8fortune8fatedress@gmail.com>
2021-04-07 19:23:01 +0900

    ✨ ヘッダーの追加

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Footer } from '../components/Footer'
 import styles from '../styles/Home.module.css'
 import { Main } from '../components/Main'
+import { Header } from '../components/Header'
 
 export default function Home() {
   return (
@@ -10,9 +11,8 @@ export default function Home() {
         <title>About Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
       <Main page="about" />
-
       <Footer />
     </div>
   )
